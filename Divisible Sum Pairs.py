6e743079aa0e7ab587e5c714f7c9aa6b61a5f525
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-01-14 18:13:19 -0500

    Create Divisible Sum Pairs.py

---
+++ Divisible Sum Pairs.py
@@ -0,0 +1,12 @@
+def divisibleSumPairs(n, k, ar):
+    x = 0
+    counter = 0
+    while x < n:
+        y = x+1
+        while x < y & y < n:
+            sum = ar[x] + ar[y]
+            if sum % k == 0:
+                counter += 1  
+            y += 1
+        x += 1            
+    return counter
