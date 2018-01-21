01b655b0a8d293971d5fbbcac5ee701a3f2d5af3
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-01-21 17:49:03 -0500

    Update drawing_Book.py

--- drawing_Book.py
+++ drawing_Book.py
@@ -1,15 +1,16 @@
 #!/bin/python3
 # https://www.hackerrank.com/challenges/drawing-book/problem
 
-import sys
-
 def solve(n, p):
     start = p//2
     end = (n-p)//2
+    if (n-p) == 1 and n%2 == 0 and p > 1:
+        return 1
     if start < end:
         return start
     else:
         return end
+    
 
 n = int(input().strip())
 p = int(input().strip())
