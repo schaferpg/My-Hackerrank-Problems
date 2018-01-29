ae4916c6c3019d33e774db7529b82d57b894185c
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-01-28 21:57:57 -0500

    Update picking_Number.py

--- picking_Number.py
+++ picking_Number.py
@@ -7,9 +7,9 @@ def pickingNumbers(a):
     large = 0
     
     # Solved this by counting the total number of times a number appears in the array added to the total number of times the number 
-    # that is one above it. Example: 1 and 2, in sample input 0 appears once and 2 appears 0 times so count = 1. Using one more 
+    # that is one above it. Example: 1 and 2, in sample input 0, 1 appears once while 2 appears zero times so count = 1. Using one more 
     # example 3 and 4: 3 appears two times in the sample input 0 and 4 appears once. So count = 3 and I used an if statement to 
-    # ensure I got the largest number.
+    # ensure I got the largest count each time it ran through the loop.
     
     while x <= max(a):
         count = 0
