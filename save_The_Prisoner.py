403f6aa8f99c9241b2e2014fe133257342516443
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-01-30 18:20:31 -0500

    Update save_The_Prisoner.py

--- save_The_Prisoner.py
+++ save_The_Prisoner.py
@@ -4,25 +4,12 @@
 
 import sys
 
-def save_The_Prisoner(n, m, s):
-    # This allows me to find the prisoners Id number
-    prison_Id = m + s - 1
-    #
-    nums = []
-    # Make's a list the prisoner's Ids
-    for i in range(n):
-        i += 1
-        nums.append(i)
-    # Not sure what logic I can use in this case but the while loop seems kinda ugly.
-    # I'm trying to make sure it the program subtracts the number of prisoners 
-    # from the prison_Id until there's a match to a prisoner's id.
-    x = 2
-    while True:
-        if prison_Id not in nums:
-            prison_Id -= n
-        else:
-            False
-            return prison_Id
+def saveThePrisoner(n, m, s):
+    change = (m+s-1)%n
+    if change == 0:
+        return n
+    else:
+        return change
 
     
     
