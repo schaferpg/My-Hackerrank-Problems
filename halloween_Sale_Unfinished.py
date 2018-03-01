f22692a661d89d68109355284a72ba122860b1d3
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-03-01 15:44:33 -0500

    Update halloween_Sale_Unfinished.py

--- halloween_Sale_Unfinished.py
+++ halloween_Sale_Unfinished.py
@@ -1,14 +1,17 @@
 # https://www.hackerrank.com/challenges/halloween-sale/problem
 import sys
 
-def howManyGames(p, d, m, s):
+def how_Many_Games(p, d, m, s):
     total = counter = 0
     price = p
+    # This while loop makes sure that the total being spent does not exceed the money you have.
     while total <= s:
         total += price
+        # This double checks what the while loop does.
         if total >= s:
             break
         price -= d
+        # This makes sure that the price doesn't go below what the store has set as the price floor.
         if price <= m:
             price = m
         counter += 1
@@ -17,5 +20,5 @@ def howManyGames(p, d, m, s):
 if __name__ == "__main__":
     p, d, m, s = input().strip().split(' ')
     p, d, m, s = [int(p), int(d), int(m), int(s)]
-    answer = howManyGames(p, d, m, s)
+    answer = how_Many_Games(p, d, m, s)
     print(answer)
