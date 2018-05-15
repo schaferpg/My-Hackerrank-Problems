928d898a4936767960fff6ebd37f9df4f2784155
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-05-15 16:47:32 -0400

    Create 2D_Array.py

---
+++ 2D_Array.py
@@ -0,0 +1,33 @@
+# Final prettier Solution
+
+sum_Arr = []
+
+
+for i in range(4):
+    for j in range(4):
+        sum_Arr.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
+        
+print(max(sum_Arr))
+
+###
+# What I orginally had
+
+def array2D(arr):
+    sum_Arr = []
+    #Vertical compenent, keeps track of where to start in the 6x6 matrix
+    for i in range(4):
+        #Horizontal copenent, same purpose as vertical
+        for j in range(4):
+            sum_Arr.append(get_Total(i,j))
+    sum_Arr.sort()
+    return sum_Arr[len(sum_Arr)-1]
+# Finds the hourgalss sum of wherever it starts at in the 6x6 matrix
+def get_Total(vert, horiz):
+    total = 0
+    for i in range(3):
+        if i == 0 or i == 2:
+            for j in range(3):
+                total += arr[vert + i][horiz + j]
+        else:
+            total += arr[vert + 1][horiz + 1]
+    return total
