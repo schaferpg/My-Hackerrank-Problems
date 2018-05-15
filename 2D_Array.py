808aa28fa302743815d138c6ebe9a6157c738c56
schaferpg <34200341+schaferpg@users.noreply.github.com>
2018-05-15 16:51:14 -0400

    Update 2D_Array.py

--- 2D_Array.py
+++ 2D_Array.py
@@ -4,8 +4,6 @@
 
 def array2D(arr):
     sum_Arr = []
-
-
     for i in range(4):
         for j in range(4):
             sum_Arr.append(arr[i][j]+arr[i][j+1]+arr[i][j+2]+arr[i+1][j+1]+arr[i+2][j]+arr[i+2][j+1]+arr[i+2][j+2])
